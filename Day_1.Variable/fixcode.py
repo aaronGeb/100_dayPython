864647eb1c3e8e7a522fe432893011cb1240e44f
Xmuluneh <mulabr673@gmail.com>
2022-06-10 11:04:29 +0300

    Bug fix

---
+++ Day_1.Variable/fixcode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" print(Day 1 -String Manipulation")
+print("String Concatenation is done with the "+" sign")
+   print('e.g print("Hello "+" "world)')
+print(("New line can be created with a backslash and n.) 
+
+"""
+print("Day 1 -String Manipulation")
+print("String Concatenation is done with the "+" sign")
+print('e.g print("Hello "+" "world)')
+print(("New line can be created with a backslash and n.")) 
