3928234417349bc1fa07ce117efcf3478075c36a
Xmuluneh <mulabr673@gmail.com>
2022-06-10 13:04:47 +0300

    final Day 1 task

---
+++ Day_1.Variable/final.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+1.create a greeting for your program
+2.Ask the user for the city that they grew up in
+3.Ask the user for the name of a pet.
+4.Combine the name of their city and pet and show them their band name
+5.make sure the input curser show on a new line 
+"""
+print("Welcome to the band generator")
+city_name = input("Enter your birth place please\n")
+pet_name = input("Enter the name of your pet\n")
+print("Your birth place is {0}  and your pet is {1} ".format(city_name,pet_name))
