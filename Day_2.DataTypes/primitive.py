82739b5facfe39b40078464218bb84ae7ffd1e7f
Xmuluneh <mulabr673@gmail.com>
2022-06-11 10:04:03 +0300

    day 2 task

---
+++ Day_2.DataTypes/primitive.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Write a programme tah add the digit in a2 digit number
+eg. 35 = 3=5 =8
+"""
+
+two_digit_number =input("Type a two digit number:")
+digits =int(two_digit_number[0]) +int(two_digit_number[1])
+print("The sum of the digit is :",digits)
