2bca661a976824673a56b4411f8ab10a554ab2bf
Xmuluneh <mulabr673@gmail.com>
2022-06-11 12:03:22 +0300

    tip bill task completed

--- Day_2.DataTypes/dayTwofinal.py
+++ Day_2.DataTypes/dayTwofinal.py
@@ -8,4 +8,4 @@ tip_waiter =eval(input("What percentage tip would you like to give? 10, 12, or 1
 number_of_people =eval(input("how many people to split the bill?"))
 sum_total = total_amount+total_amount*tip_waiter*0.01
 total_bill =round(((total_amount+total_amount*(tip_waiter/100))/number_of_people),2)
-print("The bill is {} and the tip percent is {} % so,the total bill is {} and we share it with {} and each pays {} dollar ".format(total_amount,tip_waiter,sum_total,number_of_people,total_bill))
+print("The bill is {} and the tip percent is {} % so,the total bill is {} and we share it with {} and each pays {:.2f} dollar ".format(total_amount,tip_waiter,sum_total,number_of_people,total_bill))
