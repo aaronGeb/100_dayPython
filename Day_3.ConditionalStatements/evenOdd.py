57e16e09af2d672392164a68ce8daf196a60265a
Xmuluneh <mulabr673@gmail.com>
2022-06-12 11:49:22 +0300

    EvenOdd task

---
+++ Day_3.ConditionalStatements/evenOdd.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+check the user input evn or odd 
+"""
+user_input = eval(input("Enter your number:"))
+if user_input % 2==0:
+    print("The number is even ")
+else:
+    print("The number is odd")   
