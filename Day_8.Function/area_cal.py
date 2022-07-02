496fdf1547deb1bdab15e2a5c3febc6819a71b44
Xmuluneh <mulabr673@gmail.com>
2022-07-02 10:18:40 +0300

    Day 8

--- Day_8.Function/area_cal.py
+++ Day_8.Function/area_cal.py
@@ -4,6 +4,7 @@ Instruction on the paint can says that 1- can can cover 5/m2 of wall
 Give a random height and width calculate how many cans of paint you'll need to buy
 
 """
+import math as m
 def can_value(height, width,can_cover):
     return (height*width)/can_cover
 
@@ -11,5 +12,5 @@ def can_value(height, width,can_cover):
 wall_height = int(input("Enter the wall height:"))
 wall_width = int(input("Enter the wall width:"))
 can_cover = int(input("Enter the can covering area:"))
-number_can_need = round(can_value(wall_height,wall_width,can_cover))
+number_can_need = m.ceil(can_value(wall_height,wall_width,can_cover))
 print("You need {} number of can".format(number_can_need))
