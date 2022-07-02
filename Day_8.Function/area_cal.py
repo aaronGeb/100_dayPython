4cbb2474d591a96c4e658370b661e6a44b8e849a
Xmuluneh <mulabr673@gmail.com>
2022-07-02 10:15:43 +0300

    Day 8

---
+++ Day_8.Function/area_cal.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A painting wall .
+Instruction on the paint can says that 1- can can cover 5/m2 of wall
+Give a random height and width calculate how many cans of paint you'll need to buy
+
+"""
+def can_value(height, width,can_cover):
+    return (height*width)/can_cover
+
+
+wall_height = int(input("Enter the wall height:"))
+wall_width = int(input("Enter the wall width:"))
+can_cover = int(input("Enter the can covering area:"))
+number_can_need = round(can_value(wall_height,wall_width,can_cover))
+print("You need {} number of can".format(number_can_need))
---
+++ Day_8.Function/greet.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Position and key argument"""
+def greet(name, location):
+    print("Hello:"+name+"How is it the:"+location)
+
+
+
+
+location = input("Enter your name")
+name =input("Enter your location")
+greet(location=name, name =location)   
